21acde7d3887a157b975fed4dd35781bb46b27b2
Enver <enverdavid01@gmail.com>
2021-08-19 14:48:12 -0500

    listo el header

--- script.js
+++ script.js
@@ -1,11 +1,33 @@
-const menuBtn = document.getElementById("mobile-cta");
-const closeMenuButton = document.getElementById("mobile-exit");
-const nav = document.querySelector("nav");
 
-menuBtn.addEventListener("click", () => {
-  nav.classList.add("menu-active");
-});
+const switchMenu = () => {
+    const menuItems = document.querySelector(".menu-items");
+    menuItems.classList.toggle("show");
+}
 
-closeMenuButton.addEventListener("click", () => {
-  nav.classList.remove("menu-active");
+addEventListener("DOMContentLoaded", () => {
+    const btnMenu = document.querySelector(".btn-menu");
+    if (btnMenu) {
+      btnMenu.addEventListener("click", () => {
+        switchMenu()
+      });
+    }
+  });
+
+const ul = document.querySelector(".menu-items");
+
+
+ul.addEventListener('click', () => {
+    switchMenu()
+})
+
+// mientras
+document.addEventListener('scroll', () => {
+    const menu = document.querySelector(".menu");
+
+    if (window.scrollY > 0) {
+        menu.classList.add("menu-background");
+    } else {
+        menu.classList.remove("menu-background");
+    }
+
 });
