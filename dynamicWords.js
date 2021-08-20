a62223bb09ca54c84a6e84d4b0ce70fd54bfd80a
Enver <enverdavid01@gmail.com>
2021-08-20 13:23:32 -0500

    lista la primera iteración en develop

---
+++ dynamicWords.js
@@ -0,0 +1,15 @@
+const typed = new Typed(".typed", {
+    strings: [
+        'Developer',
+        'Full-Stack',
+    ],
+    typeSpeed: 75,
+    startDealay: 300,
+    backSpeed: 75,
+    backDelay: 1500,
+    loop: true,
+    loopCount: false,
+    showCursor: true,
+    cursorChar: '.',
+    contentType: 'html'
+});
